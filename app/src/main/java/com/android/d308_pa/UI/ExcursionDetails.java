fc3daa8f6567a905473e1e7a86e0450ae467c57f
Cole McDougal <xmrmcdougal@gmail.com>
2026-02-21 10:11:46 -0600

    Modified the homepage to include a login feature.

--- app/src/main/java/com/android/d308_pa/UI/ExcursionDetails.java
+++ app/src/main/java/com/android/d308_pa/UI/ExcursionDetails.java
@@ -37,7 +37,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-public class ExcursionDetails extends AppCompatActivity {
+public class  ExcursionDetails extends AppCompatActivity {
 
     String title;
     Double price;
--- app/src/main/java/com/android/d308_pa/UI/MainActivity.java
+++ app/src/main/java/com/android/d308_pa/UI/MainActivity.java
@@ -2,8 +2,11 @@ package com.android.d308_pa.UI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,11 +25,23 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         Button button=findViewById(R.id.button);
+        EditText userNameInput = findViewById(R.id.username_input);
+        EditText passwordInput = findViewById(R.id.password_input);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this, VacationList.class);
-                startActivity(intent);
+                String username = userNameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                Log.i("Test Credentials", "Username: " + username + " and Password: " + password);
+                if ((username.isEmpty()) && (password.isEmpty())) {
+                    Toast.makeText(MainActivity.this, "Please enter a username and password!", Toast.LENGTH_LONG).show();
+                }
+                else if ((username.equals("username")) && (password.equals("password"))) {
+                    Intent intent = new Intent(MainActivity.this, VacationList.class);
+                    startActivity(intent);                }
+                else {
+                    Toast.makeText(MainActivity.this, "Invalid username and password!", Toast.LENGTH_LONG).show();
+                }
             }
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
