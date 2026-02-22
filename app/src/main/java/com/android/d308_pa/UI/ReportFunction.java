e9ae494086b5aba9d1357d404d722250cb40bd43
Cole McDougal <xmrmcdougal@gmail.com>
2026-02-22 08:48:21 -0600

    Had to adjust some UI elements, now done with B

--- app/src/main/java/com/android/d308_pa/UI/ReportFunction.java
+++ app/src/main/java/com/android/d308_pa/UI/ReportFunction.java
@@ -1,6 +1,9 @@
 package com.android.d308_pa.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -32,6 +35,14 @@ public class ReportFunction extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button button = findViewById(R.id.report_return);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ReportFunction.this, VacationList.class);
+                startActivity(intent);
+            }
         });
 
         TextView dateCreated = findViewById(R.id.report_timestamp);
